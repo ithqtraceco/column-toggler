f6c6975d1a547d5cc796a63bbbf8d54ffd6aeb74
ithqtraceco <traceco@ithq.qc.ca>
2026-04-20 13:01:28 -0400

    Add Vite configuration for Vue and Laravel Nova

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+export default defineConfig({
+plugins: [vue()],
+build: {
+  outDir: 'dist',
+  lib: {
+	entry: path.resolve(__dirname, 'resources/js/tool.js'),
+	name: 'ColumnToggler',
+	fileName: (format) => `js/tool.js`,
+	formats: ['iife'],
+  },
+  rollupOptions: {
+	external: ['vue', 'laravel-nova'],
+	output: {
+	  globals: {
+		vue: 'Vue',
+		'laravel-nova': 'LaravelNova',
+	  },
+	  assetFileNames: (assetInfo) => {
+		if (assetInfo.name === 'style.css') return 'css/tool.css'
+		return assetInfo.name
+	  },
+	},
+  },
+},
+resolve: {
+  alias: {
+	'laravel-nova-ui': path.resolve(__dirname, '/application-ithq/vendor/laravel/nova/resources/ui/components'),
+  },
+},
+})
